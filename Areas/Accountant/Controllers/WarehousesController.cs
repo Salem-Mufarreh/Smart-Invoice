ffa44b1ed5da645b6f245d79c9060cac4e304537
Salem Mufarreh <salemmuf9@gmail.com>
2023-05-14 21:16:11 +0300

    created Controllers for newly added models

---
+++ Areas/Accountant/Controllers/WarehousesController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Smart_Invoice.Data;
+using Smart_Invoice.Models.Warehouse;
+
+namespace Smart_Invoice.Areas.Accountant.Controllers
+{
+    [Area("Accountant")]
+    public class WarehousesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WarehousesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Accountant/Warehouses
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Warehouses.Include(w => w.Product);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Accountant/Warehouses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Warehouses == null)
+            {
+                return NotFound();
+            }
+
+            var warehouse = await _context.Warehouses
+                .Include(w => w.Product)
+                .FirstOrDefaultAsync(m => m.WarehouseId == id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+
+            return View(warehouse);
+        }
+
+        // GET: Accountant/Warehouses/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId");
+            return View();
+        }
+
+        // POST: Accountant/Warehouses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("WarehouseId,WarehouseName,WarehouseCode,Address,Capacity,AvailableSpace,OccupancyRate,Status,ProductId")] Warehouse warehouse)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(warehouse);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", warehouse.ProductId);
+            return View(warehouse);
+        }
+
+        // GET: Accountant/Warehouses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Warehouses == null)
+            {
+                return NotFound();
+            }
+
+            var warehouse = await _context.Warehouses.FindAsync(id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", warehouse.ProductId);
+            return View(warehouse);
+        }
+
+        // POST: Accountant/Warehouses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("WarehouseId,WarehouseName,WarehouseCode,Address,Capacity,AvailableSpace,OccupancyRate,Status,ProductId")] Warehouse warehouse)
+        {
+            if (id != warehouse.WarehouseId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(warehouse);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WarehouseExists(warehouse.WarehouseId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", warehouse.ProductId);
+            return View(warehouse);
+        }
+
+        // GET: Accountant/Warehouses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Warehouses == null)
+            {
+                return NotFound();
+            }
+
+            var warehouse = await _context.Warehouses
+                .Include(w => w.Product)
+                .FirstOrDefaultAsync(m => m.WarehouseId == id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+
+            return View(warehouse);
+        }
+
+        // POST: Accountant/Warehouses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Warehouses == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Warehouses'  is null.");
+            }
+            var warehouse = await _context.Warehouses.FindAsync(id);
+            if (warehouse != null)
+            {
+                _context.Warehouses.Remove(warehouse);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WarehouseExists(int id)
+        {
+          return (_context.Warehouses?.Any(e => e.WarehouseId == id)).GetValueOrDefault();
+        }
+    }
+}
